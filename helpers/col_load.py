4ec13916948c877948c7e1a973f814b2ef7b37c0
Wouter-VDP <woutervdp@gmail.com>
2019-11-14 17:07:25 -0500

    v2

--- helpers/col_load.py
+++ helpers/col_load.py
@@ -12,6 +12,11 @@ col_trk = [
     "trk_pid_chimu_v_v",
     "trk_pid_chipr_u_v",
     "trk_pid_chimu_u_v",
+    "trk_llr_pid_u_v",
+    "trk_llr_pid_v_v",
+    "trk_llr_pid_y_v",
+    "trk_llr_pid_v",
+    "trk_llr_pid_score_v",
 ]
 col_shr = [
     "shr_dist_v",
@@ -25,10 +30,17 @@ col_shr = [
     "shr_tkfit_dedx_u_v",
     "shr_tkfit_dedx_v_v",
     "shr_tkfit_dedx_y_v",
+    "shr_tkfit_gap10_dedx_u_v",
+    "shr_tkfit_gap10_dedx_v_v",
+    "shr_tkfit_gap10_dedx_y_v",
     "shr_tkfit_nhits_v",
     "shr_tkfit_dedx_nhits_u_v",
     "shr_tkfit_dedx_nhits_v_v",
     "shr_tkfit_dedx_nhits_y_v",
+    #"shr_nclus0_v", -> Not filled for every pfp, need to be changed!
+    #"shr_nclus0_v",
+    #"shr_nclus0_v",
+    #"shr_clushitfrac2_v"
 ]
 col_backtracked = [
     "backtracked_pdg",
@@ -51,6 +63,9 @@ col_mc = [
     "interaction",
     "weightSpline",
     "leeweight",
+    
+    "nu_completeness_from_pfp",
+    "nu_purity_from_pfp"
 ]
 col_event = [
     "run",
@@ -67,11 +82,20 @@ col_event = [
     "reco_nu_vtx_y",
     "reco_nu_vtx_z",
     "crtveto",
+    "crthitpe",
     "slclustfrac",
     "hits_ratio",
     "nu_flashmatch_score",
-    "contained_fraction",
+    'selected',
+    "category",
+    "nslice",
+    "CosmicIP",
+    "n_tracks_contained",
+    "n_tracks_contained"
+    # contained_sps_ratio -> currently not availible
 ]
+
+cols_run3 = ["_closestNuCosmicDist"]
 
 cols_flatten = col_trk + col_shr + col_pfp
 cols_reco = col_event + cols_flatten
@@ -81,7 +105,6 @@ table_cols = [
     "mc_pdg",
     "mc_E",
     "ccnc",
-    "category",
     "theta",
     "true_nu_vtx_x",
     "true_nu_vtx_y",
@@ -90,4 +113,12 @@ table_cols = [
     "interaction",
     "weightSpline",
     "leeweight",
+    "category",
+    "nu_e",
+    "nu_pdg",
+    'nelec',
+    'npion',
+    'npi0',
+    'nproton',
+    'nmuon'
 ]
---
+++ helpers/plot_dicts.py
@@ -0,0 +1,93 @@
+category_labels = {
+    1: r"$\nu_e$ CC other",
+    10: r"$\nu_e$ CC 0$\pi$0p",
+    11: r"$\nu_e$ CC 0$\pi$Np",
+    111: r"MiniBooNE LEE",
+    2: r"$\nu_{\mu}$ CC other",
+    21: r"$\nu_{\mu}$ CC $\pi^{0}$",
+    3: r"$\nu$ NC",
+    31: r"$\nu$ NC $\pi^{0}$",
+    4: r"Cosmic",
+    5: r"Out of FV",
+    6: r"other",
+    0: r"No slice"
+}
+
+pdg_labels = {
+    2212: r"$p$",
+    13: r"$\mu$",
+    11: r"$e$",
+    111: r"$\pi^0$",
+    -13: r"$\mu$",
+    -11: r"$e$",
+    211: r"$\pi^{\pm}$",
+    -211: r"$\pi$",
+    2112: r"$n$",
+    22: r"$\gamma$",
+    321: r"$K$",
+    -321: r"$K$",
+    0: "Cosmic"
+}
+
+int_labels = {
+    0: "QE",
+    1: "Resonant",
+    2: "DIS",
+    3: "Coherent",
+    4: "Coherent Elastic",
+    5: "Electron scatt.",
+    6: "IMDAnnihilation",
+    7: r"Inverse $\beta$ decay",
+    8: "Glashow resonance",
+    9: "AMNuGamma",
+    10: "MEC",
+    11: "Diffractive",
+    12: "EM",
+    13: "Weak Mix"
+}
+
+
+int_colors = {
+    0: "bisque",
+    1: "darkorange",
+    2: "goldenrod",
+    3: "lightcoral",
+    4: "forestgreen",
+    5: "turquoise",
+    6: "teal",
+    7: "deepskyblue",
+    8: "steelblue",
+    9: "royalblue",
+    10: "crimson",
+    11: "mediumorchid",
+    12: "magenta",
+    13: "pink",
+    111: "black"
+}
+
+category_colors = {
+    4: "xkcd:light red",
+    5: "xkcd:brick",
+    2: "xkcd:cyan",
+    21: "xkcd:cerulean",
+    3: "xkcd:cobalt",
+    31: "xkcd:sky blue",
+    1: "xkcd:green",
+    10: "xkcd:mint green",
+    11: "xkcd:lime green",
+    111: "xkcd:goldenrod",
+    6: "xkcd:grey",
+    0: "xkcd:black"
+}
+
+pdg_colors = {
+    2212: "#a6cee3",
+    22: "#1f78b4",
+    13: "#b2df8a",
+    211: "#33a02c",
+    111: "#137e6d",
+    0: "#e31a1c",
+    11: "#ff7f00",
+    321: "#fdbf6f",
+    2112: "#cab2d6",
+}
